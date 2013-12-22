28a8b4405306b46ee005596a53319ffae2d7881f
Sam Sartor <eeky.sam@gmail.com>
2013-12-21 20:25:14 -0700

    Fixed Git
    I messed up some stuff

---
+++ src/Tech/tech/StoneTools.java
@@ -0,0 +1,17 @@
+package tech;
+
+import zap.turrem.tech.Tech;
+
+public class StoneTools extends Tech
+{
+	public StoneTools(int pass)
+	{
+		super(pass);
+	}
+
+	@Override
+	public String getName()
+	{
+		return "StoneTools";
+	}
+}
--- src/zap/turrem/Main.java
+++
@@ -1,9 +0,0 @@
-package zap.turrem;
-
-public class Main
-{
-	public static void main(String[] args)
-	{
-
-	}
-}
