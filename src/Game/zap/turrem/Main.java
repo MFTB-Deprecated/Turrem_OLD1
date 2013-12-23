755c73873c9463ef9f4f72fcb6d0a956cc92c902
Sam Sartor <eeky.sam@gmail.com>
2013-12-22 18:18:55 -0700

    Game Run Timer
    Times game loop

--- src/Game/zap/turrem/Main.java
+++ src/Game/zap/turrem/Main.java
@@ -1,5 +1,7 @@
 package zap.turrem;
 
+import zap.turrem.utils.StopTimer;
+
 public class Main
 {
 	public static void main(String[] args)
@@ -9,6 +11,10 @@ public class Main
 		dir += "/";
 		Turrem t = new Turrem(dir, new Session());
 		Thread.currentThread().setName("Turrem main thread");
+		StopTimer timer = new StopTimer();
+		timer.start();
 		t.run();
+		long nano = timer.end();
+		System.out.printf("Ran for %.2fs%n", nano * 0.000000001D);
 	}
 }
