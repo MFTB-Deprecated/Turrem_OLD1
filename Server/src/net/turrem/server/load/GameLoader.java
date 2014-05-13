2496ccc38a406bff7485c1f03636f881ec0e5b8c
Sam Sartor <eeky.sam@gmail.com>
2014-05-13 12:35:55 -0600

    Jar Explore
    Explore jar file

--- Server/src/net/turrem/server/load/GameLoader.java
+++ Server/src/net/turrem/server/load/GameLoader.java
@@ -1,15 +1,32 @@
 package net.turrem.server.load;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import net.turrem.server.TurremServer;
 
 public class GameLoader
 {
 	protected TurremServer theServer;
 	protected EntityLoader loaderEntity;
+	public ClassLoader theClassLoader;
 	
 	public GameLoader(TurremServer server)
 	{
 		this.theServer = server;
+		this.theClassLoader = this.theServer.getClass().getClassLoader();
 		this.loaderEntity = new EntityLoader(this);
+	}
+	
+	public void loadJar(File jar)
+	{
+		JarExplore explore = JarExplore.newInstance(jar, this.theClassLoader);
+		ArrayList<Class<?>> classList = explore.loadJarClassFiles();
+		this.processClassList(classList);
+	}
+	
+	private void processClassList(ArrayList<Class<?>> classList)
+	{
+		
 	}
 }
--- Server/src/net/turrem/server/load/JarExplore.java
+++ Server/src/net/turrem/server/load/JarExplore.java
@@ -2,22 +2,87 @@ package net.turrem.server.load;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class JarExplore
 {
 	protected File theFile;
-	protected JarFile jar;
-	protected GameClassLoader theLoader;
-	
-	private JarExplore(File jarFile, GameClassLoader loader) throws IOException
+	protected JarFile jar = null;
+	protected GameClassLoader theLoader = null;
+	protected ClassLoader theSuperLoader;
+
+	private JarExplore(File jarFile, ClassLoader loader) throws IOException
 	{
 		this.theFile = jarFile;
-		this.theLoader = loader;
+		this.theSuperLoader = loader;
 		this.jar = new JarFile(this.theFile);
 	}
-	
-	public static JarExplore newInstance(File jarFile, GameClassLoader loader)
+
+	public ArrayList<Class<?>> loadJarClassFiles()
+	{
+		try
+		{
+			this.theLoader = new GameClassLoader(new URL[] { this.theFile.toURI().toURL() }, this.theSuperLoader);
+		}
+		catch (MalformedURLException e)
+		{
+			return null;
+		}
+
+		if (this.theLoader != null && this.jar != null)
+		{
+			ArrayList<Class<?>> found = new ArrayList<Class<?>>();
+
+			for (JarEntry je : Collections.list(this.jar.entries()))
+			{
+				if (!je.isDirectory())
+				{
+					String className = this.getClassName(je.getName());
+					Class<?> theClass;
+					try
+					{
+						theClass = this.theLoader.loadClass(className);
+					}
+					catch (ClassNotFoundException e)
+					{
+						System.err.println(className + ".class could not be loaded from \"" + this.theFile.getName() + "\"");
+						e.printStackTrace();
+						theClass = null;
+					}
+					if (theClass != null)
+					{
+						found.add(theClass);
+					}
+				}
+			}
+			
+			try
+			{
+				this.jar.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+
+			return found;
+		}
+		return null;
+	}
+
+	public String getClassName(String name)
+	{
+		String className = name.substring(0, name.lastIndexOf('.'));
+		className = className.replace('/', '.');
+		return className;
+	}
+
+	public static JarExplore newInstance(File jarFile, ClassLoader loader)
 	{
 		if (!jarFile.exists())
 		{
